f633ee86d477862291c20f33367eed0ee082c37f
levan-iakobidze <levaniakobidze25@gmail.com>
2022-05-31 22:14:48 +0400

    responsive finished

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,11 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Categories.css";
 import Container from "../Container/Container";
+import { allData } from "./Market";
+import { foodData } from "./Market";
+import { coffeeData } from "./Market";
+import { dessertsData } from "./Market";
 
 function Categories() {
+  const [categorie, setCategorie] = useState("all");
+
+  const all = categorie === "all";
+  const food = categorie === "food";
+  const coffee = categorie === "coffee";
+  const desserts = categorie === "desserts";
+
   return (
     <section className='categories'>
-      <Container className='categories-cont'></Container>
+      <Container className='categories-cont'>
+        <div className='categories-menu'>
+          <h1>Categories</h1>
+          <ul>
+            <li className={all && "active"} onClick={() => setCategorie("all")}>
+              All
+            </li>
+            <li
+              className={food && "active"}
+              onClick={() => setCategorie("food")}>
+              Food
+            </li>
+            <li
+              className={coffee && "active"}
+              onClick={() => setCategorie("coffee")}>
+              Coffee
+            </li>
+            <li
+              className={desserts && "active"}
+              onClick={() => setCategorie("desserts")}>
+              Deserts
+            </li>
+          </ul>
+        </div>
+
+        <div className='cards-cont'>
+          {all &&
+            allData.map((food) => {
+              return (
+                <div className={all ? "card active-card" : "card"}>
+                  <img src={food.img} alt='ramen' />
+                </div>
+              );
+            })}
+          {food &&
+            foodData.map((food) => {
+              return (
+                <div className={food ? "card active-card" : "card"}>
+                  <img src={food.img} alt='ramen' />
+                </div>
+              );
+            })}
+
+          {coffee &&
+            coffeeData.map((food) => {
+              return (
+                <div className={coffee ? "card active-card" : "card"}>
+                  <img src={food.img} alt='ramen' />
+                </div>
+              );
+            })}
+          {desserts &&
+            dessertsData.map((food) => {
+              return (
+                <div className={desserts ? "card active-card" : "card"}>
+                  <img src={food.img} alt='ramen' />
+                </div>
+              );
+            })}
+        </div>
+      </Container>
     </section>
   );
 }
---
+++ src/components/Categories/Market.js
@@ -0,0 +1,71 @@
+export const allData = [
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+  {
+    img: "https://images.pexels.com/photos/3535383/pexels-photo-3535383.jpeg?auto=compress&cs=tinysrgb&w=600",
+    name: "Steak",
+    price: 20,
+  },
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+];
+
+export const foodData = [
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+];
+
+export const coffeeData = [
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+];
+
+export const dessertsData = [
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+  {
+    img: "https://images.pexels.com/photos/1555814/pexels-photo-1555814.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    name: "Ramen",
+    price: 20,
+  },
+];
